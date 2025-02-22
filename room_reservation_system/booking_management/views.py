79160f0e821e28073ba002579bff64aecdebcdd5
Kamil Giszka <kamilgiszka109@gmail.com>
2025-02-22 16:43:31 +0100

    passing errors to add_room.html improved

--- room_reservation_system/booking_management/views.py
+++ room_reservation_system/booking_management/views.py
@@ -1,5 +1,5 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from booking_management.models import Room
 
@@ -11,12 +11,12 @@ class AddRoomView(View):
     def post(self, request):
         room_name = request.POST.get('room_name')
         if not room_name:
-            return HttpResponse('Room Name cannot be empty')
+            return render(request, 'add_room.html', context={'error': 'No room name'})
         if Room.objects.filter(name=room_name).exists():
-            return HttpResponse('Room name already exists')
+            return render(request, 'add_room.html', context={'error': 'Room already exists'})
         room_capacity = int(request.POST.get('room_capacity'))
         if room_capacity < 1:
-            return HttpResponse('Room capacity must be greater than 0')
+            return render(request, 'add_room.html', context={'error': 'Capacity must be greater than 0'})
         projector_availability = request.POST.get('projector_availability') == 'on'
         Room.objects.create(name=room_name, room_capacity=room_capacity, projector_availability=projector_availability)
-        return render(request, 'index.html')
+        return redirect('all_rooms')
